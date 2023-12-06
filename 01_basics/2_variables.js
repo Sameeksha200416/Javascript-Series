fecfc2811f50119a467e94f6ae8bae8d39037ba8
sameeksha vishwakarma <122034943+Sameeksha200416@users.noreply.github.com>
2023-12-06 10:14:36 +0000

    DataTypes

---
+++ 01_basics/2_variables.js
@@ -0,0 +1,25 @@
+"use strict "; //treat all JS code as newer version
+
+//alert(3+3)   // we are using nodejs,not browser
+
+console.log(3+3)    //code readblity should be high
+console.log("sam")
+
+//datatype
+let name = "samee"
+let age = 10
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 53
+//bigint       //it is use for very very big numer like in tradding
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//undefined =>
+//symbol => unqiue
+
+//obejct
+console.log(typeof undefined);  //undefined
+console.log(typeof null);  //object   ........so that type of null is object 
