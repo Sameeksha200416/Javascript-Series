44c4264d75926995fb3531b34565153b98758211
sameeksha vishwakarma <122034943+Sameeksha200416@users.noreply.github.com>
2023-12-08 04:33:55 +0000

    conversion of datatype

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,5 +24,59 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// **********************************OPERATIONS**********************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);
+
+let str1 ="hello"
+let str2 = "sam"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 +2);
+// console.log(1 + 2 +"2");//this is applyble but not correct way and this is ,messy way to write code
+// console.log((3+4)*5%3);
+
+console.log(true);
+//or
+console.log(+true);
+console.log(+"");
+
+let num1,num2, num3
+num1 = num2 = num3= 2+2
+
+let gameCounter = 100
+//gameCounter++;
+//or
+++gameCounter;
+
+console.log(gameCounter);
+
+//********Prefix and Postfix**************
+//Postfix = x++
+//Prefix = ++x
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+
+let a = 3;
+const b = ++a;
+
+console.log(`x:${a}, y:${b}`);
