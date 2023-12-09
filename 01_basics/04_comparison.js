19d92fc732e44308196d0a798cc975f4082b22c8
sameeksha vishwakarma <122034943+Sameeksha200416@users.noreply.github.com>
2023-12-09 10:25:22 +0000

    Conversion

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+//Basic conversion
+// console.log(2 >1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2 == 1);
+// console.log(2!=1);
+
+// conversion here one side string and another side different datatype
+// console.log("2">1);
+// console.log("02">1);
+
+
+//always type clean code
+//this type of plz avoid becz this type of code conversion 
+//create confusion and not a right way.
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); 
+
+// === this is strick check symbol
+console.log("2" === 2);
