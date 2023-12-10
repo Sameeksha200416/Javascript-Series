6a4edbcb33609efc40ce12a6d963a8e6ed7ffc3b
sameeksha vishwakarma <122034943+Sameeksha200416@users.noreply.github.com>
2023-12-10 10:38:58 +0000

    Summary

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,37 @@
+//Javascript is dynamic type language
+// # primitive
+
+// 7 types of primitive datatype:
+// String,Number, Bollean,Null,Undefined ,Symbol,BigInt
+
+const score = 100
+const scoreValue = 100.4
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = symbol('123')
+
+console.log(id === anotherId);
+
+const heros = ["Shaktiman","naagraj","doga"]
+let myobj ={
+    name: "sam",
+    age: 19,
+}
+
+
+// Reference Type (Non-Primtive)
+
+//Array, Objects, Functions
+
+//function
+const myFunction = function(){
+    console.log("hello world!");
+}
+
+console.log(typeof bigNumber);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
