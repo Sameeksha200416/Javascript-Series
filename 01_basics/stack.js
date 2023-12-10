3bc864ffa653ae3bfe42cad0546becc736287e99
sameeksha vishwakarma <122034943+Sameeksha200416@users.noreply.github.com>
2023-12-10 14:47:04 +0000

    stack type

---
+++ 01_basics/stack.js
@@ -0,0 +1,23 @@
+//Stack(primitive), Heap(Non-Primitive)
+
+let myYoutubename = "hiteshchoudharydotcom"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "sam@gmail.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
