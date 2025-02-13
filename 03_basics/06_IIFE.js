b31d6f6eaa5f01de17f56675d729108993c1690a
sameeksha vishwakarma <122034943+Sameeksha200416@users.noreply.github.com>
2025-02-13 18:12:56 +0530

    Update 06_IIFE.js

--- 03_basics/06_IIFE.js
+++ 03_basics/06_IIFE.js
@@ -1,4 +1,6 @@
-//Immediately Invoked Function Expressions(IIFE)
+//Immediately Invoked Function Expressions(IIFE) -->  are JavaScript functions that are executed immediately after 
+//they are defined. They are typically used to create a local scope for variables to prevent them from polluting the global scope.
+
 
 (function chai(){
     // name IIFE
